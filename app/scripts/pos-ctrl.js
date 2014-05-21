6c56581fde9982a0886ccfae48e8da62c5f31190
Martin Christov <martin.christov@gmail.com>
2014-05-21 18:23:45 +0300

    working on search

--- app/scripts/pos-ctrl.js
+++ app/scripts/pos-ctrl.js
@@ -1,70 +1,76 @@
 /* global angular */
 'use strict';
+(function(){
+	var POS = function(appData, UIService, $timeout){
+		this.categories = appData.data.menu.data;
+		this.currentCategoryIndex = 0;
+		
+		this.shown = 'categories';
+		this.groups = this.categories[0].groups.data;
+		this.searchResults = null; //todo this
+		this.bills = null;
 
-var POS = function(appData, UIService, $timeout){
-	this.categories = appData.data.menu.data;
-	this.currentCategoryIndex = 0;
-	
-	this.shown = 'categories';
-	this.groups = this.categories[0].groups.data;
-	this.bills = null;
+		this.dragging = UIService.isDragging();
+		UIService.tables = appData.data.tables.data;
+		POS.$timeout = $timeout;
 
-	this.dragging = UIService.isDragging();
-	UIService.tables = appData.data.tables.data;
-	POS.$timeout = $timeout;
-};
-POS.prototype.show = function(index){
-	var _this = this;
-	function switchCat () {
-		_this.currentCategoryIndex = index;
-		_this.groups = _this.categories[index].groups.data;
-	}
-	if(this.bills.allUp){
-		switchCat();
-		POS.$timeout(function(){_this.bills.allUp=false;},100);
-	} else {
-		switchCat();
-	}
-};
-POS.prototype.toggleBills = function(){
-	if(this.shown==='bills'){
-		this.shown='categories';
-	}
-	else {
-		this.shown='bills';
-		this.bills.current=-1;
-	}
-};
-
-POS.prototype.fullscreen = function(){
-	var doc = angular.element('html')[0],
-		ison=(document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen);
-    
-    if(ison){
-		if(doc.requestFullscreen){
-			doc.requestFullscreen();
-		} else if (doc.webkitRequestFullscreen){
-		    doc.webkitRequestFullscreen();
-		} else if (doc.mozRequestFullScreen) {
-		    doc.mozRequestFullScreen();
-		} else if (doc.msRequestFullscreen) {
-		    doc.msRequestFullscreen();
+		this.search='';
+		this.searching=false;
+	};
+	POS.prototype.show = function(index){
+		var _this = this;
+		function switchCat () {
+			_this.currentCategoryIndex = index;
+			_this.groups = _this.categories[index].groups.data;
 		}
-    }
-    else {
-		if (document.cancelFullScreen) {
-			document.cancelFullScreen();
-		} else if (document.webkitCancelFullScreen) {
-		    document.webkitCancelFullScreen();
-		} else if (document.mozCancelFullScreen) {
-		    document.mozCancelFullScreen();
-		} else if (document.msCancelFullScreen) {
-		    document.msCancelFullScreen();
+		if(this.bills.allUp){
+			switchCat();
+			POS.$timeout(function(){_this.bills.allUp=false;},100);
+		} else {
+			switchCat();
 		}
-    }
-	
-};
+	};
+	POS.prototype.toggleBills = function(){
+		if(this.shown==='bills'){
+			this.shown='categories';
+		}
+		else {
+			this.shown='bills';
+			this.bills.current=-1;
+		}
+	};
 
-POS.$inject = ['appData','UIService', '$timeout'];
+	POS.prototype.fullscreen = function(){
+		var doc = angular.element('html')[0],
+			ison=(document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen);
+	    
+	    if(ison){
+			if(doc.requestFullscreen){
+				doc.requestFullscreen();
+			} else if (doc.webkitRequestFullscreen){
+			    doc.webkitRequestFullscreen();
+			} else if (doc.mozRequestFullScreen) {
+			    doc.mozRequestFullScreen();
+			} else if (doc.msRequestFullscreen) {
+			    doc.msRequestFullscreen();
+			}
+	    }
+	    else {
+			if (document.cancelFullScreen) {
+				document.cancelFullScreen();
+			} else if (document.webkitCancelFullScreen) {
+			    document.webkitCancelFullScreen();
+			} else if (document.mozCancelFullScreen) {
+			    document.mozCancelFullScreen();
+			} else if (document.msCancelFullScreen) {
+			    document.msCancelFullScreen();
+			}
+	    }
+		
+	};
 
-angular.module('getix').controller('POS',POS);
+	POS.$inject = ['appData','UIService', '$timeout'];
+
+	angular.module('getix').controller('POS',POS);
+
+})();
